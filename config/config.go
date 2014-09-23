45a9da5a48568eb30d5e3454aaf7142cec8ad5f8
Jonathan Hartman <j@p4nt5.com>
2014-09-23 12:48:13 -0700

    Add a package for parsing JSON config files

---
+++ config/config.go
@@ -0,0 +1,81 @@
+// Author:: Jonathan Hartman (<j@p4nt5.com>)
+//
+// Copyright (C) 2014, Jonathan Hartman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+Package config implements a simple struct and some functions for configuring
+the application from a JSON config file. A proper configuration file should
+contain:
+
+    {
+      "supermarket": {
+        "universe_url": "Path to Supermarket's `universe.json` file"
+      },
+      "twitter": {
+        "api_key": "Your Twitter API key, aka 'consumer key'",
+        "api_secret": "Your Twitter API secret, aka 'consumer secret'",
+        "access_token": "Your Twitter access token",
+        "access_token_secret": "Your Twitter access token secret"
+      }
+    }
+*/
+package config
+
+import (
+  "encoding/json"
+  "io"
+  "os"
+)
+
+// SupermarketConfig implements a data structure for config items specific to
+// the Supermarket instance being connected to.
+type SupermarketConfig struct {
+  UniverseURL string `json:"universe_url"`
+}
+
+// TwitterConfig imeplements a data structure for config items specific to
+// Twitter.
+type TwitterConfig struct {
+  APIKey            string `json:"api_key"`
+  APISecret         string `json:"api_secret"`
+  AccessToken       string `json:"access_token"`
+  AccessTokenSecret string `json:"access_token_secret"`
+}
+
+// Struct Config implements a data structure for an application configuration.
+type Config struct {
+  Supermarket SupermarketConfig `json:"supermarket"`
+  Twitter     TwitterConfig     `json:"twitter"`
+}
+
+// New initializes and returns a new Config struct derived from the passed-in
+// config file path.
+func New(filename string) (c *Config, err error) {
+  c = new(Config)
+  f, err := os.Open(filename)
+  if err != nil {
+    return
+  }
+  defer f.Close()
+  err = decodeJSON(f, c)
+  return
+}
+
+// decodeJSON accepts an IO reader and a Config struct and populates that struct
+// with the data read in.
+func decodeJSON(r io.Reader, c *Config) (err error) {
+  decoder := json.NewDecoder(r)
+  return decoder.Decode(c)
+}
---
+++ config/config_test.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+  "io/ioutil"
+  "os"
+  "strings"
+  "testing"
+)
+
+var config_json = `
+  {
+    "supermarket": {
+      "universe_url": "https://example.com"
+    },
+    "twitter": {
+      "api_key": "abc",
+      "api_secret": "123",
+      "access_token": "doremi",
+      "access_token_secret": "youandme"
+    }
+  }
+`
+
+func Test_New_1(t *testing.T) {
+  f, err := ioutil.TempFile("", "supermarket-circular-config-test")
+  if err != nil {
+    t.Fatalf("Expected no error, got: %s", err)
+  }
+  f.WriteString(config_json)
+  defer f.Close()
+  defer os.Remove(f.Name())
+
+  c, err := New(f.Name())
+  if err != nil {
+    t.Fatalf("Expected no error, got: %s", err)
+  }
+  for k, v := range map[string]string{
+    c.Supermarket.UniverseURL: "https://example.com",
+    c.Twitter.APIKey: "abc",
+    c.Twitter.APISecret: "123",
+    c.Twitter.AccessToken: "doremi",
+    c.Twitter.AccessTokenSecret: "youandme",
+  } {
+    if k != v {
+      t.Fatalf("Expected: %s, got: %s", k, v)
+    }
+  }
+}
+
+func Test_decodeJSON_1(t *testing.T) {
+  c := new(Config)
+  r := strings.NewReader(config_json)
+
+  err := decodeJSON(r, c)
+  if err != nil {
+    t.Fatalf("Expected no error, got: %s", err)
+  }
+  for k, v := range map[string]string{
+    c.Supermarket.UniverseURL: "https://example.com",
+    c.Twitter.APIKey: "abc",
+    c.Twitter.APISecret: "123",
+    c.Twitter.AccessToken: "doremi",
+    c.Twitter.AccessTokenSecret: "youandme",
+  } {
+    if k != v {
+      t.Fatalf("Expected: %s, got: %s", k, v)
+    }
+  }
+}
