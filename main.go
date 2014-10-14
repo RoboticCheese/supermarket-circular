1640158f11e735c7e147645058a8d6a3bae7db9c
Jonathan Hartman <j@p4nt5.com>
2014-10-13 17:10:16 -0700

    Convert to use Goulash

--- main.go
+++ main.go
@@ -1,49 +1,58 @@
 package main
 
 import (
-  "log"
-  "os"
-  "time"
-  "github.com/RoboticCheese/supermarket-circular/cookbook_collection"
+	"github.com/RoboticCheese/goulash/api_instance"
+	"github.com/RoboticCheese/goulash/universe"
+	"github.com/RoboticCheese/supermarket-circular/config"
+	"log"
+	"os"
+	"time"
 )
 
-const (
-  API_ENDPOINT = "https://supermarket.getchef.com"
-  UNIVERSE = "universe.json"
-)
+func log_cookbooks(u *universe.Universe) {
+	for _, cb_data := range u.Cookbooks {
+		for _, ver_data := range cb_data.Versions {
+			log.Printf("Cached cookbook '%v', version '%v'...",
+				cb_data.Name,
+				ver_data.Version)
+		}
+	}
+}
 
 func main() {
-  log.SetOutput(os.Stdout)
+	log.SetOutput(os.Stdout)
 
-  url := API_ENDPOINT + "/" + UNIVERSE
-  log.Printf("Starting up with URL '%s'...", url)
+	conf, err := config.New("config.json")
+	if err != nil {
+		log.Fatalf("Error reading config: '%v'", err)
+	}
+	log.Printf("Starting up with URL '%v'...", conf.Supermarket.Endpoint)
 
-  log.Printf("Initializing current state of cookbook universe...")
-  collection, err := new(cookbook_collection.CookbookCollection).NewFromUniverse(url)
-  if err != nil {
-    log.Fatal(err)
-  }
-  for _, cb := range collection.Cookbooks {
-    log.Printf("Read cookbook: '%s', versions: '%s'", cb.Name, cb.Versions)
-  }
-  log.Printf("Initialization complete!")
+	api, err := api_instance.New(conf.Supermarket.Endpoint)
+	if err != nil {
+		log.Fatalf("Error connecting to Supermarket API: '%v'", err)
+	}
+	log.Printf("Connection established to API at '%v'...", api.Endpoint)
 
-  for ;; {
-    log.Printf("Update complete! Sleeping for 60s...")
-    time.Sleep(time.Second * 60)
+	log.Printf("Initializing current state of cookbook universe...")
+	univ, err := universe.New(api)
+	if err != nil {
+		log.Fatalf("Error initializing universe: %v", err)
+	}
+	log_cookbooks(univ)
 
-    log.Printf("Checking for updated cookbooks...")
-    updates, err := collection.Update()
-    if err != nil {
-      log.Printf("WARNING: Error occurred during update: '%s'", err)
-      continue
-    }
-    for _, cb := range updates.Cookbooks {
-      for _, version := range cb.Versions {
-        log.Printf("Found new entry, cookbook: '%s', version: '%s'",
-                   cb.Name,
-                   version)
-      }
-    }
-  }
+	for {
+		log.Print("Update complete! Sleeping for 60s...")
+		time.Sleep(time.Second * 60)
+
+		log.Print("Checking for updated cookbooks...")
+		pos_diff, _, err := univ.Update()
+		if err != nil {
+			log.Printf("WARNING: Error during update: '%v'", err)
+		} else if pos_diff != nil {
+			log_cookbooks(pos_diff)
+		} else {
+			log.Print("No updates found!")
+		}
+	}
 }
